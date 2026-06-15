7972cce2b4d86a962f622ec92009c608161dfd0a
서울민트초코 <minseong130502@gmail.com>
2026-06-15 18:27:30 +0900

    Configure Flask app to use environment variables

--- server/app.py
+++ server/app.py
@@ -42,4 +42,7 @@ def check():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_ENV') != 'production'
+    app.run(debug=debug, host='0.0.0.0', port=port)
